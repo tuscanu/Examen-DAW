09dafa59e2f8b1b673e5dfb48809dbd7be66813b
tuscanu <122150195+tuscanu@users.noreply.github.com>
2024-02-03 20:01:14 +0200

    update
    update

--- Examen backend/Examen backend/obj/Release/net8.0/Examen backend.AssemblyInfo.cs
+++ Examen backend/Examen backend/obj/Release/net8.0/Examen backend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+545648fea9a36df7193feb0593eefd19a06319f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen backend")]
---
+++ Proiect Backend/DTO/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Proiect_Backend.DTO
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public Profile Profile { get; set; } 
+    }
+
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+        public string FullName { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } 
+    }
+
+}
